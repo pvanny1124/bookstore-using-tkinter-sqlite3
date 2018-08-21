bc6e40024ea88583c83e9ec0c7ea83271044e086
pvanny1124 <pvanny1124@gmail.com>
2018-08-21 02:57:29 -0400

    update backend.py

--- backend.py
+++ backend.py
@@ -1,48 +1,37 @@
 import sqlite3
 
-def connect():
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)") #don't have to pass in autoincrement manually
-    conn.commit()
-    conn.close()
-
-def insert(title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO book VALUES(NULL, ?, ?, ?, ?)", (title, author, year, isbn)) #null creates id automatically
-    conn.commit()
-    conn.close()
-
-def view():
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-
-def search(title="", author="", year="", isbn=""): #default parameters handle empty arguments for select query
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn)) #null creates id automatically
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-
-def delete(id):
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM book WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
+class Database:
     
-def update(id, title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)") #don't have to pass in autoincrement manually
+        self.conn.commit()
 
-connect() #runs on import at frontend.py
+    def insert(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO book VALUES(NULL, ?, ?, ?, ?)", (title, author, year, isbn)) #null creates id automatically
+        self.conn.commit()
+
+    def view(self):
+        self.cur.execute("SELECT * FROM book")
+        rows = self.cur.fetchall()
+        return rows
+
+    def search(self, title="", author="", year="", isbn=""): #default parameters handle empty arguments for select query
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn)) #null creates id automatically
+        rows = self.cur.fetchall()
+        return rows
+
+    def delete(self, id):
+        self.cur.execute("DELETE FROM book WHERE id=?", (id,))
+        self.conn.commit()
+        
+    def update(self, id, title, author, year, isbn):
+        self.cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+        self.conn.commit()
+
+    def __del__(self): #executed when this instance is deleted
+        self.conn.close()
+
+
 
